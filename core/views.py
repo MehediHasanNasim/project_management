db4f90b174efc0fb363b6c5591744da5d6954859
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2022-04-04 14:46:04 +0600

    home page

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
+from core import models 
+from .models import Project, Customer, Tools 
 
 
 def home(request):
-    diction= {}
+    project_list = Project.objects.order_by('project_name')
+    diction= {'project_list':project_list}
     return render(request, 'core/home.html', context=diction) 
+
 
